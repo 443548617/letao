a10a908e43122c90674fcea31becf6bad21b3e83
443548617 <you@example.com>
2019-08-02 13:05:46 +0800

    ajax登录校验完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,5 +1,12 @@
 $(function(){
   $("#form").bootstrapValidator({
+    // 指定校验时的图标显示，默认是bootstrap风格
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    // 指定校验字段
     fields:{
       username:{
         validators:{
@@ -12,6 +19,9 @@ $(function(){
             min:2,
             max:6,
             message:"用户名长度必须在2-6之间"
+          },
+          callback:{
+            message:"用户名不存在"
           }
         }
       },
@@ -26,9 +36,44 @@ $(function(){
           min:6,
           max:12,
           message:"密码长度必须在6-12之间"
+        },
+        callback:{
+          message:"密码错误"
         }
        }
       }
     }
   })
+
+  //2.登录功能
+  $("#form").on('success.form.bv', function (e) {
+    e.preventDefault();
+    //使用ajax提交逻辑
+    $.ajax({
+      type:"post",
+      url:"/employee/employeeLogin",
+      data:$("#form").serialize(),
+      dataType:"json",
+      success:function(info){
+         console.log( info );
+         if(info.success){
+          location.href = "index.html";
+         }
+         if(info.error === 1000){
+          //  alert("当前用户名不存在");
+          $("#form").data('bootstrapValidator').updateStatus("username","INVALID","callback");
+         }
+         if(info.error === 1001){
+          // alert("密码错误");
+          $("#form").data('bootstrapValidator').updateStatus("password","INVALID","callback");
+        }
+
+      }
+    });
+});
+
+//3 重置功能
+$("[type=reset]").click(function(){
+  $("#form").data('bootstrapValidator').resetForm();
+});
 })
