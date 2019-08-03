167d131623c881dfeb971938abc6429429159d75
443548617 <you@example.com>
2019-08-03 16:47:53 +0800

    二级分类—-分页渲染完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -1,28 +1,39 @@
+/**
+ * Created by Jepson on 2018/8/19.
+ */
 
-$(function(){
+$(function() {
+
   var currentPage = 1;
   var pageSize = 5;
-  render();
-  function render(){
-    $.ajax({
-      type:"get",
-      url:"/category/queryTopCategoryPaging",
-      data:{
-        page:currentPage,
-        pageSize:pageSize
-      },
-      dataType:"json",
-      success:function(info){
-        console.log( info );
-        var htmlStr = template("firstTpl",info);
-        $(".lt_content tbody").html(htmlStr);
 
-        $("#pagintor").bootstrapPaginator({
-          bootstrapMajorVersion:3,//默认是2，如果是bootstrap3版本，这个参数必填
-          currentPage:info.page,//当前页
-          totalPages:Math.ceil(info.total / info.size),//总页数
-          onPageClicked:function(event, originalEvent, type,page){
-            //为按钮绑定点击事件 page:当前点击的按钮值
+  // 1. 一进入页面, 发送ajax请求, 请求一级分类数据, 进行页面渲染
+  render();
+  function render() {
+    $.ajax({
+      type: "get",
+      url: "/category/queryTopCategoryPaging",
+      data: {
+        page: currentPage,
+        pageSize: pageSize
+      },
+      dataType: "json",
+      success: function( info ) {
+        console.log( info )
+        var htmlStr = template( "firstTpl", info );
+        $('.lt_content tbody').html( htmlStr );
+
+        // 分页插件初始化
+        $('#paginator').bootstrapPaginator({
+          // 指定bootstrap版本号
+          bootstrapMajorVersion: 3,
+          // 当前页
+          currentPage: info.page,
+          // 总页数
+          totalPages: Math.ceil( info.total / info.size ),
+          // 给页码添加点击事件
+          onPageClicked: function( a, b, c, page ) {
+            // 更新当前页, 重新渲染
             currentPage = page;
             render();
           }
@@ -31,50 +42,69 @@ $(function(){
     })
   }
 
-  $("#addBtn").click(function(){
-    $("#addModal").modal("show");
+
+
+  // 2. 点击添加分类按钮, 显示添加模态框
+  $('#addBtn').click(function() {
+    // 显示添加模态框
+    $('#addModal').modal("show");
   });
 
 
-  //表单验证
-  $("#form").bootstrapValidator({
-      // 配置校验图标
+
+  // 3. 添加表单校验功能
+  $('#form').bootstrapValidator({
+
+    // 配置校验图标
     feedbackIcons: {
       valid: 'glyphicon glyphicon-ok',    // 校验成功
       invalid: 'glyphicon glyphicon-remove',  // 校验失败
       validating: 'glyphicon glyphicon-refresh' // 校验中
     },
-    fields:{
-      //配置字段
-      categoryName:{
-        //配置规则
-        validators:{
-          notEmpty:{
-            message:"不能为空"
+
+    // 字段列表
+    fields: {
+      categoryName: {
+        // 校验规则, 要求非空
+        validators: {
+          notEmpty: {
+            message: "请输入一级分类名称"
           }
+        }
       }
     }
-   }
   });
-  
-  //注册表单验证成功事件
-  $("#form").on("success.form.bv",function(e){
-     e.preventDefault();
-     $.ajax({
-       type:"POST",
-       url:"/category/addTopCategory",
-       data:$(form).serialize(),
-       dataType:"json",
-       success:function( info ){
-          console.log(info);
-          if(info.success){
-            currentPage = 1;
-            $("#addModal").modal("hide");
-            render();
 
-            $('#form').data("bootstrapValidator").resetForm(true);
-       }
-     }
-     });
-  });
+
+  // 4. 添加表单校验成功事件, 阻止默认的表单提交, 通过ajax进行提交
+  $('#form').on("success.form.bv", function( e ) {
+    // 阻止默认的提交
+    e.preventDefault();
+
+    // 通过 ajax 提交
+    $.ajax({
+      type: "post",
+      url: "/category/addTopCategory",
+      data: $('#form').serialize(),
+      dataType: "json",
+      success: function( info ) {
+        console.log( info )
+
+        if ( info.success ) {
+          // 添加成功
+          // 关闭模态框
+          $('#addModal').modal("hide");
+          // 重新渲染第一页
+          currentPage = 1;
+          render();
+
+          // 重置表单内容和状态
+          $('#form').data("bootstrapValidator").resetForm(true);
+        }
+
+      }
+    })
+  })
+
+
 });
