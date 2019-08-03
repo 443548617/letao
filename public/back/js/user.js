8833361d8d3265ae717fc5ab9e0b45bc3aae3dbb
443548617 <you@example.com>
2019-08-03 13:06:00 +0800

    用户管理-分页渲染完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,18 +1,34 @@
+ var currentPage = 1;
+ var pageSize = 5;
 
 $(function(){
-  $.ajax({
-    type:"get",
-    url:"/user/queryUser",
-    data:{
-      page:1,
-      pageSize:5
-    },
-    dataType:"json",
-    success:function(info){
-       console.log(info);
-       //准备数据
-       var htmlStr = template("tpl",info);
-       $("tbody").html(htmlStr);
-    }
-  })
+  render();
+  function render(){
+    $.ajax({
+      type:"get",
+      url:"/user/queryUser",
+      data:{
+        page:currentPage,
+        pageSize:pageSize
+      },
+      dataType:"json",
+      success:function(info){
+         console.log(info);
+         //准备数据
+         var htmlStr = template("tpl",info);
+         $("tbody").html(htmlStr);
+         $("#pagintor").bootstrapPaginator({
+          bootstrapMajorVersion:3,//默认是2，如果是bootstrap3版本，这个参数必填
+          currentPage:info.page,//当前页
+          totalPages:Math.ceil(info.total / info.size),//总页数
+          onPageClicked:function(event, originalEvent, type,page){
+            currentPage = page;
+            render();
+          }
+        });
+      }
+    });
+  }
+  
+  
 });
