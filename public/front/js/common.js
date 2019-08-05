482e5ed94b0ac5237d62e274f8d53e9446c72f9f
443548617 <you@example.com>
2019-08-05 13:01:00 +0800

    移动端首页完成[C

---
+++ public/front/js/common.js
@@ -0,0 +1,11 @@
+//主体内容区域滚动设置
+mui('.mui-scroll-wrapper').scroll({
+  indicators: false,  //是否显示滚动条
+	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+});
+
+//轮播图设置
+var gallery = mui('.mui-slider');
+gallery.slider({
+  interval:5000//自动轮播周期，若为0则不自动播放，默认为0；
+});
