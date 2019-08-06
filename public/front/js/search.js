ced4bb385892068fdedfcfc2cfcc043a7fa909d1
443548617 <you@example.com>
2019-08-06 08:15:26 +0800

    “搜索中心-删除功能，添加功能完成

--- public/front/js/search.js
+++ public/front/js/search.js
@@ -23,7 +23,58 @@ $(function(){
 
   //功能2 清空历史记录
   $(".lt_history").on("click",".btn_empty",function(){
-    localStorage.removeItem("search_list");
+    
+    //添加确认框
+    mui.confirm("您确定要清空历史记录吗？","温馨提示",["取消","确认"],function(e){
+      if(e.index === 1){
+        localStorage.removeItem("search_list");
+        render();
+      }
+    })
+    
+  });
+
+
+  //功能3 删除单条记录
+  $(".lt_history").on("click",'.btn_del',function(){
+    var that =this;
+    //添加确认框
+    mui.confirm("您确定要清空历史记录吗？","温馨提示",["取消","确认"],function(e){
+      if(e.index === 1){
+        var index = $(that).data("index");
+        var arr = getHistory();
+        arr.splice("index",1);
+        localStorage.setItem("search_list",JSON.stringify(arr));
+        render();
+      }
+    })
+    
+  });
+
+
+
+
+  //功能4 添加历史纪录功能 
+  $(".search_btn").click(function(){
+    var key = $(".search_input").val().trim();
+
+    if(key == ""){
+      alert("请输入搜索关键词");
+      return;
+    }
+    var arr = getHistory();
+    var index = arr.indexOf(key);
+    if(index != -1){
+     arr.splice(index,1)
+    }
+    if(arr.length >= 10){
+      arr.pop();
+    }
+    arr.unshift(key);
+    localStorage.setItem("search_list",JSON.stringify(arr));
     render();
-  })
+    $(".search_input").val("");
+  });
+
+
 })
